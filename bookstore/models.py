d49fe6faa93c7f982b9d4ce359efc7263c196445
sumori <hoangthiphuonguyen1602@gmail.com>
2023-04-10 09:16:22 +0700

    add model

--- bookstore/models.py
+++ bookstore/models.py
@@ -1,3 +1,53 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    cover = models.ImageField(upload_to='covers/')
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    author = models.ForeignKey('Author', on_delete=models.CASCADE)
+    published_date = models.DateField()
+    stock = models.ForeignKey('Stock', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    bio = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Review(models.Model):
+    book = models.ForeignKey('Book', on_delete=models.CASCADE)
+    review = models.TextField()
+    rating = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.review
+
+class Order(models.Model):
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    book = models.ForeignKey('Book', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.book.title
+    
+class Stock(models.Model):
+    book = models.ForeignKey('Book', on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.book.title
