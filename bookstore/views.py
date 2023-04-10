d1c40f395da2a4cf6b991ce206a4325e721161d5
sumori <hoangthiphuonguyen1602@gmail.com>
2023-04-10 13:11:33 +0700

    pull

--- bookstore/views.py
+++ bookstore/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Book, Author, Category, Review
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def getAllBooks(request, category_id, author_id, rating, price):
+
+    books = Book.objects.all()
+    context = { 
+        'books': books
+    }
+    return render(request, 'bookstore/index.html',)
