2f511253c5803241a34cfcf0ec811e1a2bcbdb62
vnhxn <99260394+vinhnhan576@users.noreply.github.com>
2023-04-18 13:51:43 +0700

    initialize home

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import BooksListView, BooksDetailView, BookCheckoutView, paymentComplete, SearchResultsListView, book_detail, AddToCar, ViewCart, RemoveItem, CheckOut, checkoutResult, RemoveAll
+from .views import BooksListView, BooksDetailView, BookCheckoutView, paymentComplete, SearchResultsListView, book_detail, AddToCar, ViewCart, RemoveItem, CheckOut, checkoutResult, RemoveAll, home
 
 urlpatterns = [
     path('', BooksListView.as_view(), name = 'list'),
+    path('home', home, name = 'home'),
     path('<int:pk>/',book_detail, name = 'detail'),
     path('<int:pk>/checkout/', BookCheckoutView.as_view(), name = 'checkout'),
     # path('checkout/', BookCheckoutView.as_view(), name = 'checkout'),
