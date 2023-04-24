f8a97ffdd99047da0100e9215b87398eec74e1e9
vnhxn <99260394+vinhnhan576@users.noreply.github.com>
2023-04-24 11:47:12 +0700

    merge

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -1,9 +1,9 @@
+from .views import BooksListView, BooksDetailView, BookCheckoutView, paymentComplete, SearchResultsListView, book_detail, AddToCar, ViewCart, RemoveItem, CheckOut, checkoutResult, RemoveAll, home, send_verification_code, Confirm, getAllOdersByUser, get_all_categories
 from django.urls import path
-from .views import BooksListView, BooksDetailView, BookCheckoutView, paymentComplete, SearchResultsListView, book_detail, AddToCar, ViewCart, RemoveItem, CheckOut, checkoutResult, RemoveAll, home
 
 urlpatterns = [
-    path('list', BooksListView.as_view(), name = 'list'),
-    path('', home, name = 'home'),
+    path('', get_all_categories, name = 'list'),
+    path('home', home, name = 'home'),
     path('<int:pk>/',book_detail, name = 'detail'),
     path('<int:pk>/checkout/', BookCheckoutView.as_view(), name = 'checkout'),
     path('complete/', paymentComplete, name = 'complete'),
@@ -14,4 +14,10 @@ urlpatterns = [
     path('remove_all/', RemoveAll,name='remove_all'),
     path('check_out/', CheckOut, name='check_out'),
     path('checkout_result/<str:email>/', checkoutResult, name='checkout_result'),
+    path('send_verification/', send_verification_code, name='send_verification'),
+    path('confirm/', Confirm, name='confirm'),
+    path('purchase-history/', getAllOdersByUser, name='purchase-history'),
+
+
+
 ]
